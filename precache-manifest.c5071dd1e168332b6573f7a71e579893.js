545e0121e9e5bd42316e1a01894e691f28cd8052
eyal karbel <eyalkerbal@gmail.com>
2020-04-08 13:54:07 +0300

    Updates

---
+++ precache-manifest.c5071dd1e168332b6573f7a71e579893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d195079850510b54eb9e0e095af6c0",
+    "url": "/OnllineChat/index.html"
+  },
+  {
+    "revision": "998c67cdc1492577b1a8",
+    "url": "/OnllineChat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df45028c7be4ec7c84df",
+    "url": "/OnllineChat/static/js/2.2bf5a0d5.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/OnllineChat/static/js/2.2bf5a0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998c67cdc1492577b1a8",
+    "url": "/OnllineChat/static/js/main.51dce03a.chunk.js"
+  },
+  {
+    "revision": "258f8bedd6c62bf48195",
+    "url": "/OnllineChat/static/js/runtime-main.36dee906.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/OnllineChat/static/media/logo.5d5d9eef.svg"
+  }
+]);
